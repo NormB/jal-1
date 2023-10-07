5f2b284938073acc045728dc976c788a1a827e02
Norm Brandinger <n.brandinger@gmail.com>
2023-10-07 13:45:09 -0400

    Replaced expect with match

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
-    test_func();
-}
+    println!("Guess the number!");
 
-fn test_func() {
-    let x = 5;
-    println!("The value of x is: {:?}", x);
-}
+    println!("Please input your guess.");
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    let mut guess = String::new();
+
+    match io::stdin().read_line(&mut guess) {
+        Ok(_) => {},
+        Err(error) => panic!("Failed to read line: {}", error),
     }
+
+    println!("You guessed: {}", guess);
 }
