75c2845b58dfa4915f9d25470215fde7231d499d
Norm Brandinger <n.brandinger@gmail.com>
2023-10-07 15:13:04 -0400

    random updates

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
 
@@ -8,7 +10,6 @@ fn main() {
     let mut guess = String::new();
 
     // read a line from stdin
-
     match io::stdin().read_line(&mut guess) {
         Ok(_) => {
             print!("You guessed: {guess}");
@@ -16,14 +17,22 @@ fn main() {
         Err(error) => panic!("Failed to read line: {}", error),
     }
 
-    println!("You guessed: {}", guess);
+    // create a list of random numbers
+    let mut rng = rand::thread_rng();
+    let mut list = [0; 10];
+    for i in 0..10 {
+        list[i] = rng.gen_range(0..100);
+    }
+    println!("Random list: {:?}", list);
 
     // create a lit of integers
-    let mut list = [42, 12, 1, 100, 15, 53, 22, 11, 1, 42];
     let sorted_list = sort(&list);
     println!("Sorted list: {:?}", sorted_list);
     sort2(&mut list);
     println!("Updated list: {:?}", list);
+
+    let l = random_string_list();
+    println!("Random string list: {:?}", l)
 }
 
 // create a function to sort a list of integers
@@ -36,4 +45,15 @@ fn sort(list: &[i32]) -> Vec<i32> {
 // create a function to sort a list of integers by updating the list
 fn sort2(list: &mut [i32]) {
     list.sort();
+}
+
+// create a function that build a list of random strings
+fn random_string_list() -> Vec<String> {
+    let mut rng = rand::thread_rng();
+    let mut list = Vec::new();
+    for _ in 0..10 {
+        let s: String = rng.gen_range(0..100).to_string();
+        list.push(s);
+    }
+    list
 }
