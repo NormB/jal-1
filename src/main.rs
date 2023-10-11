1ebecd854fa1f287a8dc3cab4a2de52dde1ac0a1
Norm Brandinger <n.brandinger@gmail.com>
2023-10-11 13:25:26 -0400

    Finish function refactoring

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,16 @@ fn main() {
     println!("Random list: {:?}", list);
 
     sort2(&mut list);
-    println!("Updated list: {:?}", list);
+    println!("Updated list (inplace): {:?}", list);
 
     let l = generate_random_strings(10, 1, 10);
     println!("Random string list: {:?}", l);
 
-    let word = random_word::gen(Lang::En);
-    println!("Random word: {word}");
+    let l = generate_random_words(10, Lang::En);
+    println!("Random word list (En): {:?}", l);
+
+    let l = generate_random_words(10, Lang::Fr);
+    println!("Random word list (Fr): {:?}", l);
 }
 
 fn generate_secret_number() -> i32 {
@@ -55,12 +58,12 @@ fn guess_secret_number() -> i32 {
     }
 }
 
-// create a function to sort a list of integers by updating the list
+// a function to sort a list of integers by updating the list
 fn sort2(list: &mut [i32]) {
     list.sort();
 }
 
-// create a function that build a list of random  strings
+// a function that build a list of random  strings
 fn generate_random_strings(count: usize, min: usize, max: usize) -> Vec<String> {
     let mut list = Vec::new();
     for _ in 0..count {
@@ -71,6 +74,15 @@ fn generate_random_strings(count: usize, min: usize, max: usize) -> Vec<String> 
             .map(char::from)
             .collect();
         list.push(s);
+    }
+    list
+}
+
+fn generate_random_words(count: usize, lang: Lang) -> Vec<String> {
+    let mut list = Vec::new();
+    for _ in 0..count {
+        let random_word = random_word::gen(lang).to_string();
+        list.push(random_word);
     }
     list
 }
